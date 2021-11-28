41eeca4d6feac56b9a9a7fc491925ad31df9b34c
cegepwx <91915850+cegepwx@users.noreply.github.com>
2021-11-28 14:54:47 -0500

    Add files via upload

---
+++ CompteClient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guichet
+{
+    abstract class CompteClient
+    {
+        private string nom;
+        private string nip;
+        private bool blocked = false;//Pour verouiller le compte du client
+        protected string numerocompte;
+
+        public string Nom { get => nom; set => nom = value; }
+        public string Nip { get => nip; set => nip = value; }
+        public bool Blocked { get => blocked; set => blocked = value; }
+
+        public CompteClient()
+        {
+           
+        }
+    }
+}
