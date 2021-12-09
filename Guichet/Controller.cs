d7fcc178b9a36c3bb810fd499d92bb4ed133f6dc
cegepwx <91915850+cegepwx@users.noreply.github.com>
2021-12-09 08:59:53 -0500

    Add files via upload

--- Guichet/Controller.cs
+++ Guichet/Controller.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace Guichet
 {
@@ -7,10 +11,7 @@ namespace Guichet
         static void Main(string[] args)
         {
             Guichet guichet = new Guichet();
-            guichet.startMachine();
-            
-
+            guichet.ouvrirguichet(10000);
         }
-        
     }
 }
